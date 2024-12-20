ea9021b5cf9cff376f34d8f83ff0d7e456a4470c
SrinathAlla18 <68918857+SrinathAlla18@users.noreply.github.com>
2024-12-20 14:09:24 -0500

    Initial commit

---
+++ src/main/java/com/srinath/dataJPA/model/Student.java
@@ -0,0 +1,12 @@
+package com.srinath.dataJPA.model;
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Student {
+}
